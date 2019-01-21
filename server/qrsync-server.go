00e36369a29cb5fbdb1b0a7748b5c782840aa82c
Michael Clapham <michaeljclapham@gmail.com>
2019-01-21 23:59:14 +0000

    Add new client to a list of clients

--- server/qrsync-server.go
+++ server/qrsync-server.go
@@ -12,11 +12,32 @@ import (
 	qrcode "github.com/skip2/go-qrcode"
 )
 
+// Client representation
+type Client struct {
+	id   string
+	name string
+}
+
+// NewClient Creates new Client
+func NewClient(id string) *Client {
+	c := new(Client)
+	c.id = id
+	c.name = ""
+	return c
+}
+
+var clientList map[string]*Client
+
 func clientHandler(w http.ResponseWriter, r *http.Request) {
 	urlParts := strings.Split(r.URL.Path, "/")
 	clientID := urlParts[len(urlParts)-1]
+	client := NewClient(clientID)
+	clientList = append(clientList, client)
 	fmt.Fprintf(w, "Hello client id %s!\n", clientID)
 	fmt.Fprintf(w, "Your path is %s", r.URL.Path)
+	for i, val := range clientList {
+		fmt.Fprintf(w, "Other client %d %s", i, &val)
+	}
 }
 
 func randomNumber() int {
@@ -28,7 +49,8 @@ var domain = flag.String("domain", "localhost", "Domain name for links")
 
 func qrHandler(w http.ResponseWriter, r *http.Request) {
 	var png []byte
-	link := fmt.Sprintf("http://%v:8080/client/c%v", *domain, randomNumber())
+	clientId := "c" + fmt.Sprint(randomNumber())
+	link := fmt.Sprintf("http://%v:8080/client/c%v", *domain, clientId)
 	fmt.Println(link)
 	png, err := qrcode.Encode(link, qrcode.Medium, 256)
 	if err != nil {
