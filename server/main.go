6f307c223bf067e947dcc378def31197d5fed098
Michael Clapham <michaeljclapham@gmail.com>
2019-09-19 14:13:27 +0100

    Change default ports

--- server/main.go
+++ server/main.go
@@ -6,8 +6,12 @@ import (
 )
 
 func main() {
-	ssl := flag.Bool("ssl", true, "Use ssl")
-	port := flag.Int("port", 443, "Port to run server on")
+	ssl := flag.Bool("ssl", false, "If true use SSL")
+	defaultPort := 4002
+	if ssl {
+		defaultPort = 4001
+	}
+	port := flag.Int("port", defaultPort, "Port to run server on")
 	flag.Parse()
 	var app = App{}
 	app.Initialize("../web/client", "../web/admin", 1)
